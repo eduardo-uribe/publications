f03ba86332fe79444192266dca4bd3708db06efa
javascripty <ed2711uribe@gmail.com>
2024-09-24 17:13:40 -0700

    Update commenting-system .html, .css, and .js

---
+++ content/return-json-in-rocket.md
@@ -0,0 +1,10 @@
++++
+title = "How to return JSON in Rocket.rs"
+date = 2024-06-01
++++
+
+[JSON](https://rocket.rs/guide/v0.5/responses/#json)
+
+[json](https://api.rocket.rs/v0.5/rocket/serde/json/struct.Json)
+
+[Enabling JSON](https://api.rocket.rs/v0.5/rocket/serde/json/)
---
+++ static/javascript/commenting-system.js
@@ -0,0 +1,179 @@
+class CommentingSystem extends HTMLElement {
+  constructor() {
+    super();
+
+    this.addEventListener('submit', async function (event) {
+      try {
+        event.preventDefault();
+
+        let parent = event.target.parentElement;
+        let ul = parent.querySelector('ul');
+
+        // new comment form data
+        let form_data = new FormData(event.target);
+
+        let new_comment_data = {
+          comment_author: form_data.get('author'),
+          comment_content: form_data.get('comment'),
+          comment_thread_id: this.getAttribute(
+            'data-commenting-system-thread-id'
+          ),
+        };
+
+        // create comment element template
+        let new_comment_html = this.create_new_comment_html(new_comment_data);
+
+        ul.prepend(new_comment_html);
+
+        if (event.target.matches('[data-commenting-system] > form')) {
+          // reset form fields
+          return event.target.reset();
+        } else {
+          // re-enable comment reply button
+          let button = event.target
+            .closest('li')
+            .querySelector('[data-reply-button]');
+
+          button.removeAttribute('disabled');
+
+          // remove form
+          event.target.remove();
+        }
+
+        let endpoint = 'http://127.0.0.1:8000/api/v1/comment';
+
+        let headers = new Headers();
+        let api_key = this.getAttribute('data-commenting-system-api-key');
+        headers.append('x-api-key', api_key);
+        headers.append('Content-Type', 'application/json');
+
+        let request = new Request(endpoint, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify(new_comment_data),
+        });
+
+        await fetch(request);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    this.addEventListener('click', async function (event) {
+      if (event.target.matches('[data-reply-button]')) {
+        // parent element
+        let parent = event.target.parentElement;
+
+        // create a reply form
+        let form = this.create_reply_form();
+        let textarea = form.querySelector('textarea');
+
+        // display reply form
+        parent.after(form);
+
+        // disable reply button
+        event.target.setAttribute('disabled', 'true');
+
+        // set focus on form text area
+        textarea.focus();
+      }
+
+      if (event.target.matches('[data-cancel-button]')) {
+        // re-enable comment reply button
+        let button = event.target
+          .closest('li')
+          .querySelector('[data-reply-button]');
+
+        button.removeAttribute('disabled');
+
+        // remove reply form
+        let form = event.target.parentElement;
+
+        form.remove();
+      }
+    });
+  }
+
+  /**
+   * Create the reply form html structure
+   * @returns
+   */
+  create_reply_form() {
+    let form = document.createElement('form');
+
+    let input_label = document.createElement('label');
+    input_label.setAttribute('for', 'author');
+    input_label.textContent = 'Author';
+
+    let input = document.createElement('input');
+    input.setAttribute('name', 'author');
+    input.setAttribute('id', 'author');
+    input.setAttribute('value', 'Internet stranger');
+
+    let textarea_label = document.createElement('label');
+    textarea_label.setAttribute('for', 'comment');
+    textarea_label.textContent = 'Comment';
+
+    let textarea = document.createElement('textarea');
+    textarea.setAttribute('name', 'comment');
+    textarea.setAttribute('id', 'comment');
+    textarea.setAttribute('required', 'true');
+
+    let submit_button = document.createElement('button');
+    submit_button.textContent = 'Submit';
+
+    let cancel_button = document.createElement('button');
+    cancel_button.setAttribute('data-cancel-button', '');
+    cancel_button.setAttribute('type', 'button');
+    cancel_button.textContent = 'Cancel';
+
+    form.append(
+      input_label,
+      input,
+      textarea_label,
+      textarea,
+      submit_button,
+      cancel_button
+    );
+
+    return form;
+  }
+
+  /**
+   * Submit a fetch request to create a new comment
+   * @param {*} param0
+   * @returns
+   */
+  create_new_comment_html({ comment_author, comment_content }) {
+    let li = document.createElement('li');
+
+    let article = document.createElement('article');
+
+    let div = document.createElement('div');
+
+    let author = document.createElement('p');
+    author.textContent = comment_author;
+
+    let datetime = document.createElement('time');
+    datetime.textContent = Date.now();
+
+    let content = document.createElement('p');
+    content.textContent = comment_content;
+
+    let button = document.createElement('button');
+    button.setAttribute('data-reply-button', '');
+    button.textContent = 'Reply';
+
+    let ul = document.createElement('ul');
+
+    div.append(author, datetime);
+
+    article.append(div, content, button);
+
+    li.append(article, ul);
+
+    return li;
+  }
+}
+
+customElements.define('commenting-system', CommentingSystem);
